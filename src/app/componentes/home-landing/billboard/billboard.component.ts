c397e0898ff7102a04534732c4a80bfcc309dcc6
SenpaiRickTFT21 <jwalkingtarget@Outlook.com>
2023-05-09 17:53:06 -0600

    animaciones

--- src/app/componentes/home-landing/billboard/billboard.component.ts
+++ src/app/componentes/home-landing/billboard/billboard.component.ts
@@ -1,32 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-billboard',
   templateUrl: './billboard.component.html',
   styleUrls: ['./billboard.component.css'],
-  animations: [
-    trigger('swipeUpIn', [
-      transition('void => *', [
-        style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate('600ms ease-in-out'),
-      ]),
-    ]),
-    trigger('swipeDownIn', [
-      transition('void => *', [
-        style({ opacity: 0, transform: 'translateY(-10px)' }),
-        animate('600ms ease-in-out'),
-      ]),
-    ]),
-  ],
 })
 export class BillboardComponent implements OnInit {
   title = 'appBootstrap';
@@ -34,7 +14,9 @@ export class BillboardComponent implements OnInit {
 
   constructor(private router: Router, private modalService: NgbModal) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    AOS.init();
+  }
 
   scrolltoDestacados(id) {
     let el = document.getElementById(id);
