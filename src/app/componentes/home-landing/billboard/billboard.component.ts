0eacbcb53e4acba51d5065348f3de64815a3c4a2
SenpaiRickTFT21 <jwalkingtarget@Outlook.com>
2023-05-08 22:39:34 -0600

    Home responsividad lista menos el menu

--- src/app/componentes/home-landing/billboard/billboard.component.ts
+++ src/app/componentes/home-landing/billboard/billboard.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-billboard',
   templateUrl: './billboard.component.html',
   styleUrls: ['./billboard.component.css'],
+  animations: [
+    trigger('swipeUpIn', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('600ms ease-in-out'),
+      ]),
+    ]),
+    trigger('swipeDownIn', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('600ms ease-in-out'),
+      ]),
+    ]),
+  ],
 })
 export class BillboardComponent implements OnInit {
   title = 'appBootstrap';
